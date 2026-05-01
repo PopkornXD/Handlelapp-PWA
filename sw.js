8a9016e3a736c2aca48e9751cbb43aee30a288b6
Iver E. Johnson <Iver@grottenveien.no>
2026-05-02 01:42:14 +0200

    .

--- sw.js
+++ sw.js
@@ -9,32 +9,37 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', event => {
+  console.log('[sw] install')
   event.waitUntil(
     caches.open(CACHE).then(cache => cache.addAll(ASSETS))
   );
 });
 
 self.addEventListener('activate', event => {
+  console.log('[sw] activate')
   event.waitUntil(
     caches.keys().then(keys => Promise.all(
       keys.map(key => key === CACHE ? null : caches.delete(key))
     ))
   );
+
+  if (self.clients && typeof self.clients.claim === 'function') {
+    event.waitUntil(self.clients.claim());
+  }
 });
 
 self.addEventListener('fetch', event => {
   const req = event.request;
 
   if (req.mode === 'navigate' || (req.method === 'GET' && req.headers.get('accept')?.includes('text/html'))) {
+    console.log('[sw] navigation request', req.url)
     event.respondWith(
       fetch(req)
         .then(networkResponse => {
           caches.open(CACHE).then(cache => {
             try {
               cache.put('/index.html', networkResponse.clone());
-            } catch (e) {
-              
-            }
+            } catch (e) { }
           });
           return networkResponse;
         })
@@ -43,6 +48,7 @@ self.addEventListener('fetch', event => {
     return;
   }
 
+  console.log('[sw] fetch', req.url)
   event.respondWith(
     caches.match(req).then(resp => resp || fetch(req))
   );
